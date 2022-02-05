8518fe9ddb9409e990b625c949ad1b4936a6b610
Permanent Record <mklersy33@gmail.com>
2022-02-05 21:57:00 +0100

    environnement update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    HOSTNAME: process.env.NODE_ENV === 'production' ? 'leny.vercel.app' : 'localhost',
+    PORT: process.env.NODE_ENV === 'production' ? '' : '3000',
+  },
 }
