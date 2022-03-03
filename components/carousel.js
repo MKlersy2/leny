a7752f1b4802980f9bbcede8744afcbf003fee1f
Permanent Record <mklersy33@gmail.com>
2022-03-03 18:22:59 +0100

    ajout des points (sous titres)

--- components/carousel.js
+++ components/carousel.js
@@ -26,15 +26,15 @@ export default function Carousel() {
                     <div className={carousel.boxHorizontal}>
                         <div className={`${styles.displayFlex} ${styles.flexCol} ${carousel.listeDesc}`}>
                             <div fadein='true' id='carouselFst' className={`${effectStyles.appear} ${effectStyles.fromRight} ${carousel.textGlobal}`}>
-                                <h3 className={`${styles.boxSubtitle} ${carousel.title}`}><span className={`${headStyle.ligature}`}>V</span>otre implication est récompensée</h3>
+                                <h3 className={`${styles.boxSubtitle} ${carousel.title}`}><span className={`${headStyle.ligature}`}>V</span>otre implication est récompensée.</h3>
                                 <p className={`${styles.boxParagraphe} ${carousel.commentaires}`}>Le LNY est la monnaie qui vous est attribuée en fonction de la note de votre agence. Cette note est définie par votre taux de data provide et votre taux de contribution. Plus vous utilisez Leny et diffusez vos biens sur la plateforme, plus votre visibilité est accrue et votre activité se voit récompensée. La cryptomonnaie LNY est directement versée dans votre wallet Metamask et vous permet de payer les abonnements Leny.</p>
                             </div>
                             <div id='carouselScd' className={`${effectStyles.fromRight} ${carousel.textGlobal}`}>
-                                <h3 className={`${styles.boxSubtitle} ${carousel.title}`}><span className={`${headStyle.ligature}`}>R</span>estez en avant</h3>
+                                <h3 className={`${styles.boxSubtitle} ${carousel.title}`}><span className={`${headStyle.ligature}`}>R</span>estez en avant.</h3>
                                 <p className={`${styles.boxParagraphe} ${carousel.commentaires}`}>Avec Leny, vous partagez avec vos clients un lien dédié à votre agence et à votre nom. Votre compte peut être multi-utilisateurs au sein de votre agence, ainsi vos agents restent un interlocuteur unique tout au long de la recherche de vos clients, en étant en mesure de proposer un pool de biens correspondants, à votre image.</p>
                             </div>
                             <div id='carouselThd' className={`${effectStyles.fromRight} ${carousel.textGlobal}`}>
-                                <h3 className={`${styles.boxSubtitle} ${carousel.title}`}><span className={`${headStyle.ligature}`}>L</span>orem ipsum</h3>
+                                <h3 className={`${styles.boxSubtitle} ${carousel.title}`}><span className={`${headStyle.ligature}`}>L</span>orem ipsum.</h3>
                                 <p className={`${styles.boxParagraphe} ${carousel.commentaires}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.</p>
                             </div>
                         </div>
--- components/carouselHorizontal.js
+++ components/carouselHorizontal.js
@@ -27,7 +27,7 @@ export default function CarouselHorizontal() {
                         <div className={`${styles.displayFlex} ${styles.flexCol} ${carouselHorizontal.listeDesc}`}>
                             <div className={carouselHorizontal.fake}></div>
                             <div fadein='true' id='carouselFst' className={`${effectStyles.appear} ${effectStyles.fromNowhere} ${carouselHorizontal.textGlobal}`}>
-                                <h3 className={`${styles.boxSubtitle} ${carouselHorizontal.title}`}><span className={`${headStyle.ligature}`}>E</span>toffez votre offre</h3>
+                                <h3 className={`${styles.boxSubtitle} ${carouselHorizontal.title}`}><span className={`${headStyle.ligature}`}>E</span>toffez votre offre.</h3>
                                 <p className={`${styles.boxParagraphe} ${carouselHorizontal.commentaires}`}>Le superpouvoir de Leny vous donne accès à la sélection de biens la plus complète du marché. L&apos;ensemble des propriétés commercialisées par les professionnels et les particuliers devient enfin accessible, sur un portail unique. LenyPlus rajoute des cordes à votre arc avec un panel toujours plus enrichi à proposer : les biens off-market du moment se dévoilent à vos clients, en toute confidentialité.</p>
                             </div>
                             <div id='carouselScd' className={`${effectStyles.fromNowhere} ${carouselHorizontal.textGlobal}`}>
@@ -35,7 +35,7 @@ export default function CarouselHorizontal() {
                                 <p className={`${styles.boxParagraphe} ${carouselHorizontal.commentaires}`}>Votre temps est précieux : Leny collecte toutes les informations nécessaires à la constitution de votre base acquéreur. L&apos;enregistrement automatique de leur critères de recherche vous permet de rester concentré sur l&apos;essentiel : votre métier.</p>
                             </div>
                             <div id='carouselThd' className={`${effectStyles.fromNowhere} ${carouselHorizontal.textGlobal}`}>
-                                <h3 className={`${styles.boxSubtitle} ${carouselHorizontal.title}`}><span className={`${headStyle.ligature}`}>D</span>evenez indispensable</h3>
+                                <h3 className={`${styles.boxSubtitle} ${carouselHorizontal.title}`}><span className={`${headStyle.ligature}`}>D</span>evenez indispensable.</h3>
                                 <p className={`${styles.boxParagraphe} ${carouselHorizontal.commentaires}`}>La magie opère quand vous utilisez Leny, l&apos;outil de croisement le plus abouti qui vous donne accès aux biens de vos confrères, des particuliers de votre ville, et aux biens &laquo; secrets &raquo;. En recevant une sélection inédite, directement par whatsapp, mail ou sms, votre client sort du brouillard. Trouvez un bien n&apos;a jamais été aussi facile.</p>
                             </div>
                         </div>
